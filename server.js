253c05c5aed184e69da9742bb7e3ceb27e257710
SotaSuzuki <3.3sota.suzuki@gmail.com>
2023-02-27 22:53:36 +0900

    add signup form

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ const port = 4000;
 
 app.use(express.static("public"));
 
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/signup", (req, res) => {
+  console.log(req.body);
+  res.send("アカウント登録しました。");
+});
+
 app.listen(port, () => {
   console.log(`Server is running on  http://localhost:${port}`);
 });
