f665f14410fa95f5cf410687b16c46dad73d7d11
SotaSuzuki <3.3sota.suzuki@gmail.com>
2023-02-27 19:00:39 +0900

    add public dir to use as static

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ const express = require("express");
 const app = express();
 const port = 4000;
 
-app.get("/", (req, res, next) => {
-  res.end("Top Page");
-});
+app.use(express.static("public"));
 
 app.listen(port, () => {
   console.log(`Server is running on  http://localhost:${port}`);
